2e7ab3649157103fa06aa4f69ec77031ab869248
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2021-04-08 23:00:02 +0300

    query elements depending on page

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
-let color = '#3aa757';
+"use strict";
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// page updated (user clicked on video etc)
+let prevUrl = "";
+
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+	let currentUrl = details.url;
+	console.log("prev: " + prevUrl + ", current: " + currentUrl);
+
+	if (currentUrl && prevUrl && currentUrl !== prevUrl) {
+		console.log("sending url: " + currentUrl);
+
+		// window.onload = (event) => {
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, {url: currentUrl}, null);
+			});
+		// };
+	}
+	prevUrl = currentUrl;
+
+}, {url: [{urlMatches : 'https://www.youtube.com'}]});
--- scripts/content.js
+++ scripts/content.js
@@ -1,145 +1,272 @@
 "use strict";
 
-// find all thumbnails
-let thumbnails = document.querySelectorAll("ytd-thumbnail");
-for (let thumbnail of thumbnails) {
+const thumbnailPath = "ytd-thumbnail a[id='thumbnail']";
 
-    // add checkbox - TODO
-    // get videoId - regex/disable_scripting
-    let href = thumbnail.querySelector("a[id='thumbnail']");
-    let url = new URL(window.location + href);
-    let videoId = url.searchParams.get("v");
+// page updated (user clicked on video etc)
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("path updated: " + message.url);
+    processUrl(message.url);
+});
 
-    console.log('videoId: ' + videoId);
-    if (videoId) {
-        thumbnail.videoId = videoId;
-        thumbnail.done = false;
+// first time load of website
+processUrl(window.location + '');
 
-        // search for thumbnail videoId in storage
-        chrome.storage.local.get(videoId, function(result) {
-            if (result[videoId]) {
-                console.log('videoId = ' + videoId + ', result = ' + JSON.stringify(result));
-                // if found - blur, add note: "watched"
-                thumbnail.style.opacity = 0.5;
-                thumbnail.done = true;
+function processUrl(url) {
+    let startTime = Date.now();
+    let youtubeHostRegex = /https?:\/\/(www\.)?youtube\.com/;
+    let path = url.replace(youtubeHostRegex, '');
+    console.log("PATH: " + path);
+
+    let elementsPath = [];
+    let containersPath = [];
+    let containerId = [];
+    let containerItem = [];
+
+    if (path.match(/^\/$/)) {
+        console.log("MATCHED HOME");
+        elementsPath = ["ytd-rich-grid-renderer ytd-rich-item-renderer ytd-rich-grid-media"];
+        containersPath = ["ytd-rich-grid-renderer #contents"];
+        containerId = [0];
+        containerItem = ["ytd-rich-item-renderer"];
+    }
+    else if (path.match(/^\/results/)) {
+        // fixme: 'people also watch' AND after it don't work
+        console.log("MATCHED HOME SEARCH");
+        elementsPath = ["ytd-section-list-renderer ytd-item-section-renderer ytd-video-renderer", "ytd-vertical-list-renderer ytd-video-renderer"];
+        containersPath = ["ytd-section-list-renderer > #contents", "ytd-vertical-list-renderer > #items"];
+        containerId = [2, 0];
+        containerItem = ["ytd-item-section-renderer", "ytd-video-renderer"];
+
+    }
+    else if (path.match(/^\/user\/[^\/]*|^\/c\/[^\/]*\/featured$/)) {
+        // fixme: only first 3 playlists work
+        console.log("MATCHED USER HOME"); //todo: observer for loading of new playlists
+        elementsPath = ["yt-horizontal-list-renderer ytd-grid-video-renderer"];
+        containersPath = ["yt-horizontal-list-renderer #items"];
+        containerId = [-1];
+        containerItem = ["ytd-grid-video-renderer"];
+    }
+    else if (path.match(/^\/c\/[^\/]*\/[^f]/)) {
+        if (path.match(/^\/c\/[^\/]*\/videos$/)) {
+            console.log("MATCHED USER VIDEOS");
+            elementsPath = ["ytd-grid-renderer ytd-grid-video-renderer"];
+            containersPath = ["ytd-grid-renderer #items"];
+            containerId = [0];
+            containerItem = ["ytd-grid-video-renderer"];
+        }
+        else if (path.match(/^\/c\/[^\/]*\/search/)) {
+            // fixme first: only first 3 videos work
+            // fixme navigation: new videos after scroll don't work
+            console.log("MATCHED USER SEARCH");
+            elementsPath = ["ytd-section-list-renderer ytd-item-section-renderer ytd-video-renderer"];
+            containersPath = ["ytd-section-list-renderer > #contents"];
+            containerId = [1];
+            containerItem = ["ytd-item-section-renderer"];
+        }
+    }
+    else if (path.match(/^\/channel\//)) {
+        // channel
+        if (path.match(/^\/channel\/[^\/]*$|^\/channel\/[^\/]*\/featured/)) {
+            console.log("MATCHED CHANNEL HOME"); //todo: observer for loading of new playlists
+            elementsPath = ["yt-horizontal-list-renderer ytd-grid-video-renderer"];
+            containersPath = ["yt-horizontal-list-renderer #items"];
+            containerId = [-1];
+            containerItem = ["ytd-grid-video-renderer"];
+        }
+        else if (path.match(/^\/channel\/[^\/]*\/videos$/)) {
+            console.log("MATCHED CHANNEL VIDEOS");
+            elementsPath = ["ytd-grid-renderer ytd-grid-video-renderer"];
+            containersPath = ["ytd-grid-renderer #items"];
+            containerId = [0];
+            containerItem = ["ytd-grid-video-renderer"];
+        }
+        else if (path.match(/^\/channel\/[^\/]*\/search/)) {
+            //fixme: on first load - processed first 3 videos
+            //fixme: didn't work on webNavigation.HistoryUpdated
+            console.log("MATCHED CHANNEL SEARCH");
+            elementsPath = ["ytd-section-list-renderer ytd-item-section-renderer ytd-video-renderer"];
+            containersPath = ["ytd-section-list-renderer > #contents"];
+            containerId = [1];
+            containerItem = ["ytd-item-section-renderer"];
+        }
+    }
+    else if (path.match(/^\/watch/)) {
+        // todo: add 'Done' for video page for convenience
+        // watch
+        if (path.match(/^\/watch\?.*list=/)) {
+            console.log("MATCHED VIDEO WITH PLAYLIST AND RELATED");
+            elementsPath = ["ytd-playlist-panel-renderer ytd-playlist-panel-video-renderer", "ytd-watch-next-secondary-results-renderer ytd-item-section-renderer ytd-compact-video-renderer"];
+            containersPath = ["ytd-playlist-panel-renderer #items", "ytd-watch-next-secondary-results-renderer ytd-item-section-renderer #contents"];
+            containerId = [0, 0];
+            containerItem = ["ytd-playlist-panel-video-renderer", "ytd-compact-video-renderer"];
+        }
+        else {
+            console.log("MATCHED VIDEO WITH RELATED");
+            elementsPath = ["ytd-watch-next-secondary-results-renderer ytd-item-section-renderer ytd-compact-video-renderer"];
+            containersPath = ["ytd-watch-next-secondary-results-renderer ytd-item-section-renderer #contents"];
+            containerId = [0];
+            containerItem = ["ytd-compact-video-renderer"];
+        }
+    }
+    else if (path.match(/^\/playlist/)) {
+        console.log("MATCHED PLAYLIST");
+        elementsPath = ["ytd-playlist-video-list-renderer ytd-playlist-video-renderer"];
+        containersPath = ["ytd-playlist-video-list-renderer #contents"];
+        containerId = [0];
+        containerItem = ["ytd-playlist-video-renderer"];
+    }
+    else if (path.match(/^\/feed/)) {
+        // feed
+        if (path.match(/^\/feed\/subscriptions$|^\/feed\/library$/)) {
+            //fixme: /feed/subscriptions?flow=2 - all don't work
+            //fixme: /feed/subscriptions - videos after scroll don't work
+            //fixme: /feed/library - 'show more' don't work
+            console.log("MATCHED FEED SUBSCRIPTIONS OR LIBRARY");
+            elementsPath = ["ytd-grid-renderer ytd-grid-video-renderer"];
+            containersPath = ["ytd-grid-renderer #items"];
+            containerId = [-1];
+            containerItem = ["ytd-grid-video-renderer"];
+        }
+        else if (path.match(/^\/feed\/explore$/)) {
+            console.log("MATCHED FEED EXPLORE");
+            elementsPath = ["ytd-expanded-shelf-contents-renderer ytd-video-renderer"];
+            containersPath = ["ytd-expanded-shelf-contents-renderer #grid-container"];
+            containerId = [0];
+            containerItem = ["ytd-video-renderer"];
+        }
+        else if (path.match(/^\/feed\/history$/)) {
+            // fixme: videos after scroll don't work
+            console.log("MATCHED FEED HISTORY");
+            elementsPath = ["ytd-section-list-renderer ytd-item-section-renderer ytd-video-renderer"];
+            containersPath = ["ytd-section-list-renderer > #contents"];
+            containerId = [0];
+            containerItem = ["ytd-item-section-renderer"];
+        }
+    }
+    else {
+        console.log("DIDN'T MATCH"); //todo: check not youtube websites
+    }
+
+    let elementPath = elementsPath[0] + ' ' + thumbnailPath;
+    console.log("TEST: " + elementPath);
+    let elements = document.querySelectorAll(elementPath);
+
+    if (!elements.length) {
+        setTimeout(() => {
+            console.log("timeout: " + (Date.now() - startTime));
+            elements = document.querySelectorAll(elementPath);
+            if (elements.length) {
+                setListeners(elementsPath, containersPath, containerId, containerItem);
             }
-        });
+        }, 10_000);
+    } else {
+        setListeners(elementsPath, containersPath, containerId, containerItem);
+    }
+}
 
-        // add event listener on video hover
-        thumbnail.addEventListener('mouseenter', {
-            handleEvent(event) {
-                let target = event.currentTarget;
-                let videoId = target.videoId;
+function setListeners(elements, containers, containerIds, containerItems) {
+    for(let i = 0; i < elements.length; i++) {
+        let elementPath = elements[i] + ' ' + thumbnailPath;
+        console.log("elementPath: " + elementPath);
+        let containerPath = containers[i];
+        console.log("containerPath: " + containerPath);
 
-                if (!target.done) {
-                    target.done = true;
-                    let obj = {}; obj[videoId] = 1;
-                    chrome.storage.local.set(obj, function() {
-                        console.log('Write ' + obj);
-                    });
-                    target.style.opacity = 0.5;
-                } else {
-                    target.done = false;
-                    chrome.storage.local.remove(videoId, function() {
-                        console.log('Delete ' + videoId);
-                    })
-                    target.style.opacity = 1;
+        let thumbnailElements = document.querySelectorAll(elementPath);
+
+        if (thumbnailElements && thumbnailElements.length) {
+            for (let thumbnail of thumbnailElements) {
+                processThumbnail(thumbnail);
+            }
+
+            let observer = new MutationObserver(mutations => callback(mutations, containerItems[i]));
+            if (containerIds[i] > -1) {
+                let container = document.querySelectorAll(containerPath)[containerIds[i]];
+                try {
+                    if (container) {
+                        observer.observe(container, {childList: true});
+                    }
+                } catch(err) {
+                    debugger;
+                }
+            } else {
+                let containers = document.querySelectorAll(containerPath);
+                if (containers) {
+                    for (let container of containers) {
+                        try {
+                            if (container) {
+                                observer.observe(container, {childList: true});
+                            }
+                        } catch(err) {
+                            debugger;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+function processThumbnail(thumbnail) {
+    if (thumbnail.videoId) {
+        console.log("OOPS! thumb has " + thumbnail.videoId);
+        return;
+    }
+    let href = thumbnail.getAttribute("href");
+    if (!href) return;
+
+    let url = new URL(window.location.origin + href);
+    let videoId = url.searchParams.get("v");
+    console.log('found videoId: ' + videoId);
+    if (!videoId) return;
+
+    thumbnail.videoId = videoId;
+    thumbnail.done = false;
+
+    chrome.storage.local.get(videoId, function(result) {
+        if (result[videoId]) {
+            console.log('Read ' + JSON.stringify(result));
+            thumbnail.style.opacity = 0.5;
+            thumbnail.done = true;
+        }
+    });
+
+    thumbnail.addEventListener('mouseenter', {
+        handleEvent(event) {
+            let target = event.currentTarget;
+            let videoId = target.videoId;
+
+            if (!target.done) {
+                target.done = true;
+                let obj = {}; obj[videoId] = 1;
+                chrome.storage.local.set(obj, function() {
+                    console.log('Save ' + JSON.stringify(obj));
+                });
+                target.style.opacity = 0.5;
+            } else {
+                target.done = false;
+                chrome.storage.local.remove(videoId, function() {
+                    console.log('Delete ' + JSON.stringify(videoId));
+                })
+                target.style.opacity = 1;
+            }
+        }
+    });
+}
+
+function callback(mutations, element) {
+    for (let mutation of mutations) {
+        for(let node of mutation.addedNodes) {
+            if (!(node instanceof HTMLElement)) continue;
+
+            if (node.matches(element)) {
+                console.log("observer processing thumbnail...");
+                let thumbnail = node.querySelector(thumbnailPath);
+                if (thumbnail) {
+                    processThumbnail(thumbnail);
                 }
             }
-        });
+        }
     }
-}
-
-
-/* jQuery("div#content").after("<p id='youtube-watched-checkbox' style='color:blue;'>WATCHED</p>");
-
-let divsIdContent = document.querySelectorAll("div#content"); // not live collection!
-
-let paragraphsIdDone = document.querySelectorAll("p#youtube-watched-checkbox"); // not live collection!
-jQuery("p#youtube-watched-checkbox").click(function() {
-    // get link ID
-    var p = jQuery(this);
-    let href = p.prev().find("a#thumbnail").attr("href");
-    var url = new URL(window.location + href);
-    var v = url.searchParams.get("v");
-
-    // get all IDs from storage
-    chrome.storage.local.get({done3: {}}, function(items) {
-        // if (!chrome.runtime.error) {
-            let holder = objectify(items.done3);
-            console.log("loaded from storage: " + JSON.stringify(items.done3));
-            console.log(holder.videos_ids);
-
-            if (check(holder,v) === "deleted") {
-                p.css('color','blue');
-            } else {
-                p.css('color','red');
-            }
-            save(holder);
-    });
-});*/
-
-function Holder() {
-    this.videos_ids = new Set();
-    this.last_updated = new Date(0);
-}
-
-function check(holder, id){
-    if (holder.videos_ids.has(id)) {
-        holder.videos_ids.delete(id);
-        return "deleted";
-    } else {
-        holder.videos_ids.add(id);
-        return "added";
-    }
-}
-
-function jsonify(holder) {
-    let holder_json = {};
-    holder_json.last_updated = holder.last_updated.getTime();
-    holder_json.videos_ids = [...holder.videos_ids];
-    return holder_json;
-}
-
-function objectify(holder_json) {
-    let holder = {};
-    holder.last_updated = new Date(holder_json.last_updated);
-    holder.videos_ids = new Set(holder_json.videos_ids);
-    return holder;
-}
-
-function save(holder){
-    holder.last_updated = new Date(Date.now());
-    let holder_json = jsonify(holder);
-    chrome.storage.local.set({done3: holder_json}, function() {
-        console.log('saved to storage : ' + JSON.stringify(holder_json));
-        console.log(holder.videos_ids);
-    });
-}
-
-function compare(holder, timestamp_utc) {
-    return holder.last_updated.getTime() > timestamp_utc;
-}
-
-function update(array, item)
-{
-    array.push(item);
-    chrome.storage.local.set({
-        list:array
-    }, function() {
-        console.log("added to list with new values");
-    });
-}
-
-function removeItemOnce(arr, value) {
-    var index = arr.indexOf(value);
-    if (index > -1) {
-        arr.splice(index, 1);
-    }
-    return arr;
-}
-
-function includes(arr, value) {
-    var index = arr.indexOf(value);
-    return index > -1;
+    //todo: on some pages (home etc - where >1 container possible) add listeners again, as not all might be loaded right away
 }
