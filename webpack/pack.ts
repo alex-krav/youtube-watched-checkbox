81abc53ab6635404210c4e92aa4c8b3ca9d74781
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2021-05-15 08:20:21 +0300

    updated crx to crx3

--- webpack/pack.ts
+++ webpack/pack.ts
@@ -1,19 +1,12 @@
 'use strict';
 
-import * as fs from 'fs';
-import * as path from 'path';
-const ChromeExtension = require('crx');
+const crx3 = require('crx3');
 
-const crx = new ChromeExtension({
-  codebase: 'http://localhost:8000/youtube-watched-checkbox.crx',
-  privateKey: fs.readFileSync('./key.pem'),
-});
-
-crx.load( path.resolve(__dirname, '../dist') )
-    .then((crx: any) => crx.pack())
-    .then((crxBuffer: any) => {
-      fs.writeFile('./dist/youtube-watched-checkbox.crx', crxBuffer, function() {});
-    })
-    .catch((err: Error)=>{
-      console.error( err );
-    });
+crx3(['./dist/manifest.json'], {
+  keyPath: './key.pem',
+  crxPath: './youtube-watched-checkbox.crx',
+  crxURL: 'http://127.0.0.1:8080/youtube-watched-checkbox.crx',
+})
+    .then(() => console.log('done'))
+    .catch(console.error)
+;
